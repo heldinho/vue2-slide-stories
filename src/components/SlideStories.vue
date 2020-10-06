620473f371cb6907b6732b3fc928c44f5bacfe9c
helder.passos <helder.passos@interfy.io>
2020-10-06 15:02:49 -0300

    init

---
+++ src/components/SlideStories.vue
@@ -0,0 +1,22 @@
+<template>
+  <div data-slide="slide" class="slide">
+    <div class="slide-items">
+      <img src="./img/img1.jpg" alt="Img 1" />
+      <img src="./img/img2.jpg" alt="Img 2" />
+      <img src="./img/img3.jpg" alt="Img 3" />
+      <img src="./img/img4.jpg" alt="Img 4" />
+    </div>
+    <nav class="slide-nav">
+      <div class="slide-thumb"></div>
+      <button class="slide-prev">Anterior</button>
+      <button class="slide-next">Próximo</button>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+</style>

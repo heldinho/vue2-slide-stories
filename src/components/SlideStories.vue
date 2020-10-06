0b069fe8adb7e2d6b9d92fd243d177be56b46cfc
helder.passos <helder.passos@interfy.io>
2020-10-06 15:05:34 -0300

    fix

--- src/components/SlideStories.vue
+++ src/components/SlideStories.vue
@@ -1,10 +1,7 @@
 <template>
   <div data-slide="slide" class="slide">
     <div class="slide-items">
-      <img src="./img/img1.jpg" alt="Img 1" />
-      <img src="./img/img2.jpg" alt="Img 2" />
-      <img src="./img/img3.jpg" alt="Img 3" />
-      <img src="./img/img4.jpg" alt="Img 4" />
+      <img v-for="{ src, alt, id } in images" :src="src" :alt="alt" :key="id" />
     </div>
     <nav class="slide-nav">
       <div class="slide-thumb"></div>
@@ -15,7 +12,13 @@
 </template>
 
 <script>
-export default {}
+export default {
+  name: 'SlideStories',
+  data: () => ({
+    active: 0
+  }),
+  methods: {}
+}
 </script>
 
 <style lang="scss" scoped>

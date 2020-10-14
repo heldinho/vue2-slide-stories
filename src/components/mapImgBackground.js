71f097ced6cf24ec5ac9f2b3c82db62591da90ad
heldinho <helderferreirapassos@gmail.com>
2020-10-14 02:43:44 -0300

    'fix'

---
+++ src/components/mapImgBackground.js
@@ -0,0 +1,15 @@
+import img1 from '../assets/img/background/img1.jpg'
+import img2 from '../assets/img/background/img2.jpg'
+import img3 from '../assets/img/background/img3.jpg'
+import img4 from '../assets/img/background/img4.jpg'
+import img5 from '../assets/img/background/img5.jpg'
+
+const images = [
+  { id: 1, src: img1, alt: 'img1.jpg' },
+  { id: 2, src: img2, alt: 'img2.jpg' },
+  { id: 3, src: img3, alt: 'img3.jpg' },
+  { id: 4, src: img4, alt: 'img4.jpg' },
+  { id: 5, src: img5, alt: 'img5.jpg' }
+]
+
+export default images
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     <SlideStories slideId="girafa" :images="images" />
+    <SlideStories slideId="background" :images="background" />
   </div>
 </template>
 
@@ -8,13 +9,15 @@
 // @ is an alias to /src
 import SlideStories from '@/components/SlideStories'
 import mapImages from '@/components/mapImages'
+import mapImgBackground from '@/components/mapImgBackground'
 
 export default {
   components: {
     SlideStories
   },
   data: () => ({
-    images: mapImages
+    images: mapImages,
+    background: mapImgBackground
   })
 }
 </script>
